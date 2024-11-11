2d533fbecde456abb34d12732a7b2b01b2a0bf39
Fajar Fadli <fajarmaftuhfadli@gmail.com>
2024-11-11 10:01:55 +0700

    Initial commit

---
+++ dotfiles/wezterm.lua
@@ -0,0 +1,22 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Catppuccin Mocha'
+
+config.font = wezterm.font 'JetBrainsMono Nerd Font'
+config.font_size = 16
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 15
+
+-- and finally, return the configuration to wezterm
+return config
---
+++ flake.nix
@@ -0,0 +1,149 @@
+{
+  description = "MacBook Air M1";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      nix-homebrew,
+      home-manager,
+    }:
+    let
+      configuration =
+        { pkgs, ... }:
+        {
+          nixpkgs.config.allowUnfree = true;
+
+          # List packages installed in system profile. To search by name, run:
+          # $ nix-env -qaP | grep wget
+          environment.systemPackages = with pkgs; [
+            neovim
+            vscode
+            nixfmt-rfc-style
+          ];
+
+          homebrew = {
+            enable = true;
+            brews = [
+              "mas"
+            ];
+            casks = [
+              "steam"
+              "arc"
+              "unnaturalscrollwheels"
+              "the-unarchiver"
+              "tidal"
+              "chatgpt"
+              "shottr"
+              "raycast"
+              "protonvpn"
+              "wezterm"
+            ];
+            onActivation.cleanup = "zap";
+          };
+
+          fonts.packages = [
+            (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+          ];
+
+          home-manager.backupFileExtension = "backup";
+          nix.configureBuildUsers = true;
+          nix.useDaemon = true;
+          users.users.frog.home = "/Users/frog";
+
+          system = {
+            defaults = {
+              dock = {
+                autohide = true;
+                persistent-apps = [
+                  "/System/Applications/Launchpad.app"
+                  "/System/Applications/System Settings.app"
+                  "/Applications/Steam.app"
+                  "/Applications/Tidal.app"
+                  "/Applications/Arc.app"
+                  "${pkgs.vscode}/Applications/Visual Studio Code.app"
+                ];
+                tilesize = 48;
+                magnification = true;
+                largesize = 64;
+              };
+              finder = {
+                AppleShowAllExtensions = true;
+                FXPreferredViewStyle = "Nlsv";
+                ShowPathbar = true;
+              };
+              loginwindow.GuestEnabled = false;
+            };
+            startup.chime = false;
+          };
+
+          # Auto upgrade nix package and the daemon service.
+          services.nix-daemon.enable = true;
+          # nix.package = pkgs.nix;
+
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
+
+          # Enable alternative shell support in nix-darwin.
+          # programs.fish.enable = true;
+          programs.zsh.enable = true;
+
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
+
+          # Used for backwards compatibility, please read the changelog before changing.
+          # $ darwin-rebuild changelog
+          system.stateVersion = 5;
+
+          # The platform the configuration will be used on.
+          nixpkgs.hostPlatform = "aarch64-darwin";
+        };
+    in
+    {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#simple
+      darwinConfigurations."mac" = nix-darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          nix-homebrew.darwinModules.nix-homebrew
+          {
+            nix-homebrew = {
+              # Install Homebrew under the default prefix
+              enable = true;
+
+              # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+              enableRosetta = true;
+
+              # User owning the Homebrew prefix
+              user = "frog";
+            };
+          }
+          home-manager.darwinModules.home-manager
+          {
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.frog = import ./home.nix;
+            };
+          }
+        ];
+      };
+
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."mac".pkgs;
+    };
+}
