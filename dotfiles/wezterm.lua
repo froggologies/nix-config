3567ad1d948d5e0fe7e18f27a0c320435aaf119a
Fajar Fadli <fajarmaftuhfadli@gmail.com>
2024-11-13 17:38:29 +0700

    feat: Add keybindings for Option-Left and Option-Right

--- dotfiles/wezterm.lua
+++ dotfiles/wezterm.lua
@@ -6,6 +6,13 @@ local config = wezterm.config_builder()
 
 -- This is where you actually apply your config choices
 
+config.keys = {
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    { key = "LeftArrow",  mods = "OPT", action = wezterm.action { SendString = "\x1bb" } },
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    { key = "RightArrow", mods = "OPT", action = wezterm.action { SendString = "\x1bf" } },
+}
+
 -- For example, changing the color scheme:
 config.color_scheme = 'Catppuccin Mocha'
 
