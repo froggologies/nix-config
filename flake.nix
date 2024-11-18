107df2667e960fbaf22496e247be4a13b5022224
Fajar Fadli <fajarmaftuhfadli@gmail.com>
2024-11-18 10:23:54 +0700

    fix: Update nixpkgs.url to nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   description = "MacBook Air M1";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
     nix-darwin = {
       url = "github:LnL7/nix-darwin";
       inputs.nixpkgs.follows = "nixpkgs";
